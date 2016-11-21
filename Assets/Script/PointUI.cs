2ff4789096847103b9f4bba39901d81a60f42580
jjchoio <im.joshuac@gmail.com>
2016-11-21 13:50:52 -0500

    second commit
    second commit

--- Assets/Script/PointUI.cs
+++ Assets/Script/PointUI.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
-using System.Collections;
+//using System.Collections;
 
 public class PointUI : MonoBehaviour 
 {
 
     PointData pointData = null; 
+
+	//variable for test
+	public GameObject point1;
+	public GameObject point2;
+	public GameObject edge;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,8 +18,21 @@ public class PointUI : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyUp(KeyCode.Q))
+		{
+			GetPointsDir(point1.transform.position,point2.transform.position);
+		}
 	
 	}
+
+	Vector3 GetPointsDir(Vector3 pt1, Vector3 pt2)
+	{
+		Vector3.Normalize(pt2 - pt1);
+		return Vector3.Normalize(pt2 - pt1);
+	}
+
+
+
 }
 
 
